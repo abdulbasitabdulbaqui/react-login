b7802fff0d5cb5acb7b44bfb0a68380296aa160d
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2025-09-03 00:24:48 +0530

    create login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div>
-      login app
+      <h3>Login form</h3>
+      <label>Email: </label>
+      <input></input>
+      <br/>
+      <br/>
+      <label>Password: </label>
+      <input></input>
+      <br/>
+      <br/>
+      <button>Login</button>
     </div>
   );
 }
